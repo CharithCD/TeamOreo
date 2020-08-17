799987eeccc818a3da5d7c78a0845a34d2ee5c9b
Duvindu-Pc <maxduvindu1999@gmail.com>
2020-08-17 11:58:28 +0530

    aaaaa

---
+++ app/src/main/java/com/example/era/grocery_Order.java
@@ -0,0 +1,14 @@
+package com.example.era;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class grocery_Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grocery__order);
+    }
+}
